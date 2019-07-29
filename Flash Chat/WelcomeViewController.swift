96c1ed179f7a6d74e7007b0e75974ea6b4e53efc
mictadeo <32759215+mictadeo@users.noreply.github.com>
2019-07-29 09:38:00 -0700

    Add files via upload

---
+++ Flash Chat/WelcomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat
+//
+//  This is the welcome view controller - the first thign the user sees
+//
+import UIKit
+import Firebase
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        //If user is logged in, by pass this screen and go straight to ChatViewController
+        if Auth.auth().currentUser != nil {
+            performSegue(withIdentifier: "goToChat", sender: self)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
